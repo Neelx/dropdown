56218eb7975ba7d6cdd1cba961be865f4fb290e1
Neelx <75723253+Neelx@users.noreply.github.com>
2024-08-01 02:32:51 +0530

    Update page.tsx

--- app/neel/page.tsx
+++ app/neel/page.tsx
@@ -1,7 +1,7 @@
 const Neel = () => {
     return ( 
         <div className="flex items-center justify-center h-screen text-5xl text-white font-semibold">
-            hello adibs
+            hello neel
         </div>
      );
 }
